05aa7ed9806151293f78304355ff75e30c038d6a
elpwc/wniko <elpwc@hotmail.com>
2021-12-23 18:55:02 +0800

    pixiv test

--- src/Result.svelte
+++ src/Result.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
+  import PixivApi from 'pixiv-api-client';
+  const pixiv = new PixivApi();
 
+  const word = 'ラブライブ';
+  pixiv.tokenRequest('code', 'codeVerfier').then(() => {
+    return pixiv
+      .searchIllust(word)
+      .then((json) => {
+        console.log(json);
+        return pixiv.requestUrl(json.next_url);
+      })
+      .then((json) => {
+        console.log(json); //next results
+      });
+  });
 </script>
 
 <div />
