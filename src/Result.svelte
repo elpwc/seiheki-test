856617091629f3ae2f5b62bc4703ce35e3219700
elpwc/wniko <elpwc@hotmail.com>
2022-01-27 14:27:34 +0800

    完成pixiv指定tag获取

--- src/Result.svelte
+++ src/Result.svelte
@@ -1,38 +1,26 @@
 <script lang="ts">
-  let httpRequest = new XMLHttpRequest();
-  httpRequest.open('POST', './get_img.php', true);
-  httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  httpRequest.send(JSON.stringify({ userid: '18240502' }));
+  const httpRequest = new XMLHttpRequest();
+
+  /**
+   * 发送请求图片url对象
+   * @param tag
+   * @param setu 0 nor18 1混合 2r18
+   */
+  const sendImageURLRequest = (tag: string, setu: number = 2) => {
+    httpRequest.open('POST', './get_img_url.php', true);
+    httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    httpRequest.send(`tag=${tag ?? '贫乳'},setu=${setu ?? 2}`);
+  };
 
   httpRequest.onreadystatechange = () => {
     if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-      var json = httpRequest.responseText;
-      console.log('result: ', json);
+      const res = JSON.parse(JSON.parse(httpRequest.responseText).data);
+      
+      console.log('result: ', res);
     }
   };
 
-  /*
-  import PixivApi from 'pixiv-api-client';
-  const pixiv = new PixivApi({
-    headers: {
-      referer: 'http://www.pixiv.net',
-    },
-  });
-
-  const word = 'ラブライブ';
-
-  pixiv.refreshAccessToken('hNiSUA6mrio-q_nQT7QwajT59InYYumthYBqh41DgSA').then(() => {
-    return pixiv
-      .searchIllust(word)
-      .then((json) => {
-        console.log(json);
-        return pixiv.requestUrl(json.next_url);
-      })
-      .then((json) => {
-        console.log(json); //next results
-      });
-  });
-  */
+  sendImageURLRequest('足');
 </script>
 
 <div />
