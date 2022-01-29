d544995a34e31c41eb1771f600afc9d6e44a2bb0
elpwc/wniko <elpwc@hotmail.com>
2022-01-30 03:32:07 +0800

    为随机涩图页添加r18type选项和原图链接

--- src/pages/RandomSetu.svelte
+++ src/pages/RandomSetu.svelte
@@ -9,8 +9,9 @@
 
   let image: Types.Setu | undefined = undefined;
   let inputvalue: string = '贫乳';
+  let r18type: 0 | 1 | 2 = 1;
   onMount(async () => {
-    image = await Utils.get_setu('贫乳', 1, ['small']);
+    image = await Utils.get_setu('贫乳', r18type, ['small']);
     console.log('image', image, image?.urls);
   });
 </script>
@@ -24,14 +25,22 @@
   <input bind:value={inputvalue} placeholder="这里输入xp" />
   <button
     on:click={async () => {
-      image = await Utils.get_setu(inputvalue, 1, ['small']);
+      image = await Utils.get_setu(inputvalue, 1, ['small', 'original']);
 
       console.log('image', image, image?.urls);
     }}>GET SETU</button
   >
 
-  <p>{image?.title + ' ' + image?.author}</p>
-  <img width="100%" height="100%" src={image?.urls.small} alt="setu desu" />
+  <div style="display:flex; justify-content:center;">
+    <input type="radio" id="R18" bind:group={r18type} value={1} /><label for="R18">R18 Only</label>
+    <input type="radio" id="Safe" bind:group={r18type} value={0} /><label for="Safe">Safe Only</label>
+    <input type="radio" id="Mix" bind:group={r18type} value={2} /><label for="Mix">Mix</label>
+  </div>
+
+  <p>{image?.title + ' - ' + image?.author}</p>
+  <a href={image?.urls.original} target="_blank">查看原图</a>
+
+  <img width="100%" height="100%" src={image?.urls.small} alt={image?.title} />
 </div>
 
 <style>
